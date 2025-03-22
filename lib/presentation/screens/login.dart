bb342d7143e3ef8e7a672077ec69433a5700b784
yerih <yerih7tup@gmail.com>
2025-03-22 23:19:07 +0100

    sign in and sign up working with firebase

--- lib/presentation/screens/login.dart
+++ lib/presentation/screens/login.dart
@@ -1,8 +1,10 @@
 
 
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/data/auth_service.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key, required this.title});
@@ -16,6 +18,46 @@ class LoginScreen extends StatefulWidget {
 
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final userController = TextEditingController();
+  final passController = TextEditingController();
+  String errorMsg = '';
+
+  void signUp() async {
+    try{
+      await authService.value.createAccount(email: userController.text, password: passController.text);
+      debugPrint("Sign up: user = ${userController.text}, password = ${passController.text}");
+      debugPrint("Sign up success");
+      setState(() {errorMsg = '';});
+    } on FirebaseAuthException catch (e){
+      debugPrint("Sign up failed: ${e.message}. user = ${userController.text}, password = ${passController.text}");
+      setState(() {errorMsg = e.message ?? 'There is an error';});
+    }catch (e){
+      debugPrint("last catch: $e");
+    }
+  }
+
+  void signIn() async {
+    try{
+      await authService.value.signIn(email: userController.text, password: passController.text);
+      debugPrint("Sign in: user = ${userController.text}, password = ${passController.text}");
+      debugPrint("Sign in success");
+      setState(() {errorMsg = '';});
+      authService.value.signOut();
+    } on FirebaseAuthException catch (e){
+      debugPrint("Sign in failed: ${e.message}. user = ${userController.text}, password = ${passController.text}");
+      setState(() {errorMsg = e.message ?? 'There is an error';});
+    } catch (e){
+      debugPrint("last catch: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    passController.dispose();
+    userController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +82,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     TextFormField(
                       keyboardType: TextInputType.emailAddress,
+                      controller: userController,
                       textInputAction: TextInputAction.next,
                       decoration: InputDecoration(
                         labelText: 'Email',
@@ -54,6 +97,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
                     TextFormField(
                       keyboardType: TextInputType.emailAddress,
+                      controller: passController,
                       textInputAction: TextInputAction.next,
                       decoration: InputDecoration(
                           labelText: 'Password',
@@ -64,18 +108,30 @@ class _LoginScreenState extends State<LoginScreen> {
                       onChanged: (String value) {},
                       validator: (value) { return value!.isEmpty ? 'Please enter password' : null;},
                     ),
+                    SizedBox(height: 40,),
+                    MaterialButton(
+                      onPressed: signIn,
+                      minWidth: double.infinity,
+                      color: Colors.deepPurple,
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+                      child: Text('Login'),
+                    ),
 
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 30.0),
-                      child: MaterialButton(
-                        onPressed: (){},
-                        minWidth: double.infinity,
-                        color: Colors.deepPurple,
-                        textColor: Colors.white,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-                        child: Text('Login'),
-                      ),
+                    MaterialButton(
+                      onPressed: signUp,
+                      minWidth: double.infinity,
+                      color: Color.fromARGB(1, 103, 58, 183),
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+                      child: Text('Sign up'),
+                    ),
+                    SizedBox(height: 10,),
+                    Text(
+                      errorMsg,
+                      style: TextStyle(color: Colors.redAccent),
                     )
+
                   ],
                 ),
               ),
