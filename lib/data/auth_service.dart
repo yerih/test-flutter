b71f62d7b05888b3d537c28723e9694f6d505522
yerih <yerih7tup@gmail.com>
2025-03-22 21:15:05 +0100

    adding auth service

---
+++ lib/data/auth_service.dart
@@ -0,0 +1,50 @@
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+
+ValueNotifier<AuthService> authService = ValueNotifier(AuthService());
+
+class AuthService{
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => firebaseAuth.currentUser;
+  Stream<User?> get authStateChanges => firebaseAuth.authStateChanges();
+
+  Future<UserCredential> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<UserCredential> createAccount({
+    required String email,
+    required String password,
+  }) async {
+    return await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    await firebaseAuth.signOut();
+  }
+
+  Future<void> resetPassword({ required String email}) async {
+    await firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> updateUsername({ required String username}) async {
+    await currentUser!.updateDisplayName(username);
+  }
+
+  Future<void> resetPassFromCurrentPass({
+    required String currentPass,
+    required String newPass,
+    required String email,
+  }) async {
+    AuthCredential credential = EmailAuthProvider.credential(email: email, password: password);
+    await currentUser!.reauthenticateWithCredential(credential);
+    await currentUser!.updatePassword(newPass);
+  }
+}
